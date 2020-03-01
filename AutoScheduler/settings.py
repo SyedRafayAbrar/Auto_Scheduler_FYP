a4765a56fd2a4439b9325b131b7c665adcbf9e5f
AbdulRafay <abrarrafau96@gmail.com>
2020-03-01 23:57:50 +0500

    settings cahnge

--- AutoScheduler/settings.py
+++ AutoScheduler/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'AutoScheduler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
